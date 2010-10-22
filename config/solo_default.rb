9f75a5aad4e779d081d4a21987b54c616a8f5eb2
Nicolaus Copernicus <copernicus>
2010-10-21 22:30:02 -0400

    Removed a mistyped comma Signed-off-by: Nicolaus Copernicus <copernicus>

--- config/solo_default.rb
+++ config/solo_default.rb
@@ -3,5 +3,5 @@ file_cache_path File.expand_path('cache',chef_dir)
 log_level :debug
 Chef::Log::Formatter.show_time = false
 cookbook_path [
-  File.expand_path('boxbooks_default/cookbooks',chef_dir),
+  File.expand_path('boxbooks_default/cookbooks',chef_dir)
 ]
